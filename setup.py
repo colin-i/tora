92e4026a213b662b9635a1a2ccceaa8faf0cbd97
bot <costin.botescu@gmail.com>
2021-04-02 20:33:47 +0300

    source additional checks

--- setup.py
+++ setup.py
@@ -8,14 +8,24 @@ from os import uname
 if uname().machine!='armv7l':
 	reqs='python-libtorrent-bin>=2.0.2'
 
+import subprocess
+import sys
+test=subprocess.run([sys.executable,'-m','pip','install',reqs])
+if test.returncode:
+	exit(test.returncode)
+import libtorrent
+from ctypes import cdll
+cdll.LoadLibrary("libgtk-4.so")
+
 import pathlib
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 from setuptools import setup
 setup(name=pkname,
-	install_requires=[reqs],
 	version='1.0.1',
+	packages=[pkname],
+	install_requires=[reqs],#still lazy for bdist_wheel
 	description='Torrent client',
 	long_description=README,
 	long_description_content_type="text/markdown",
@@ -23,7 +33,6 @@ setup(name=pkname,
 	author='bot',
 	author_email='costin.botescu@gmail.com',
 	license='MIT',
-	packages=[pkname],
 	zip_safe=False,
 	entry_points = {
 		'console_scripts': [pkname+'='+pkname+'.main:main']
