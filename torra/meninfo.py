70365e8fdfe35901eb43962aea99adafcd752ef5
bot <costin.botescu@gmail.com>
2021-05-10 19:35:27 +0300

    w,h at the right place

--- torra/meninfo.py
+++ torra/meninfo.py
@@ -10,11 +10,11 @@ def go(path,window):
 		dialog = k.gtk_dialog_new_with_buttons (b"Info",window,
 			gtk.GtkDialogFlags.GTK_DIALOG_DESTROY_WITH_PARENT | gtk.GtkDialogFlags.GTK_DIALOG_MODAL,
 			b"_OK",gtk.GtkResponseType.GTK_RESPONSE_NONE,None)
-		width=gtk.c_int()
-		height=gtk.c_int()
 		if k.gtk_window_is_maximized(window):
 			k.gtk_window_maximize(dialog)
 		else:
+			width=gtk.c_int()
+			height=gtk.c_int()
 			k.gtk_window_get_default_size (window, gtk.byref(width), gtk.byref(height))
 			k.gtk_window_set_default_size(dialog,width,height)
 		k.g_signal_connect_data (dialog,b"response",k.gtk_window_destroy,None,None,0)
