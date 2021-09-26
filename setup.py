adf0e9659f46b5297053ece16d05a7ac0cd76615
pi <costin.botescu@gmail.com>
2021-09-26 10:37:12 +0300

    pip3 install python-apt is old

--- setup.py
+++ setup.py
@@ -9,16 +9,17 @@ README = (HERE / "README.md").read_text()
 
 from setuptools import setup
 setup(name=pkname,
-	version='1.0.23',
+	version='1.0.24',
 	packages=[pkname],
 	#opt
 	python_requires='>=3',
 	install_requires=["appdirs>=1.4.3"
-		,"python-apt>=2.2.1"
+		#,"python-apt"#is from 2012 0.7.8, missing DistUtilsExtra, sudo apt install python-apt is 2.2., verify with pip3 install python-apt
 		#,'python-libtorrent-bin;platform_system=="Linux" and platform_machine=="x86_64"'
 	],
 	extras_require={
-		'bin': ['python-libtorrent-bin>=1.2.9']
+		'bin': ['python-libtorrent-bin>=1.2.9'],
+		'apt': ['python-apt']
 	},
 	description='Torrent client',
 	long_description=README,
