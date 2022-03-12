56186878229f270473e9666165aafa50f9c6fac5
bc <costin.botescu@gmail.com>
2022-03-12 07:36:53 +0200

    forcing python-libtorrent-bin for noobs at pip

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ README = (HERE / "README.md").read_text()
 
 from setuptools import setup
 setup(name=pkname,
-	version='1.0.26',
+	version='1.0.27',
 	packages=[pkname],
 	#opt
 	python_requires='>=3',
 	install_requires=["appdirs>=1.4.3"
+		,'python-libtorrent-bin>=1.2.9' #;platform_system=="Linux" and platform_machine=="x86_64"'
 		#,"python-apt"#is from 2012 0.7.8, missing DistUtilsExtra, sudo apt install python-apt is 2.2., verify with pip3 install python-apt
-		#,'python-libtorrent-bin;platform_system=="Linux" and platform_machine=="x86_64"'
 	],
-	extras_require={
-		'bin': ['python-libtorrent-bin>=1.2.9']
+	#extras_require={
+	#	'bin': ['python-libtorrent-bin>=1.2.9']
 		#,'apt': ['python-apt']
-	},
+	#},
 	description='Torrent client',
 	long_description=README,
 	long_description_content_type="text/markdown",
