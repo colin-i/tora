beb3288badf00e40f21475d6968bbf87c825a4f5
bc <costin.botescu@gmail.com>
2023-07-19 10:01:30 +0300

    cleanup

--- info.md
+++ info.md
@@ -1,4 +1,4 @@
 Torrent client.\
-To specify another torrent list at start, launch with an argument:\
-For example ```torra ~/torrents.txt```\
+To specify another torrent list at start, launch with an argument (example: ```torra ~/torrents.txt```)\
+To remove configuration launch ```torra --remove-config```.\
 [Git Page](https://github.com/colin-i/tora)
--- torra/main.py
+++ torra/main.py
@@ -3,6 +3,7 @@ torrent client
 """
 
 import sys
+import os
 
 from . import confs
 from . import gtk
@@ -38,10 +39,12 @@ def activate(app):
 	layout.layout(window)
 
 def main():
-	confs.get_root_dir().mkdir(exist_ok=True)
-
 	if len(sys.argv)>1:
-		listtor.config_filename=sys.argv[1]
+		if sys.argv[1]=="--remove-config":
+			cleanup()
+			return
+		listtor.config_filename=sys.argv[1].replace('\\','') #the same? pdb -m torra.main \\-\\-remove-config
+	confs.get_root_dir().mkdir(exist_ok=True)
 	a=k.gtk_application_new(None,0)
 	actv=b"activate"
 	k.g_signal_connect_data (a, actv, activate, None, None, 0)
@@ -49,6 +52,41 @@ def main():
 	k.g_object_unref(sets.fold_bf)
 	k.g_object_unref(a)
 	exit(r)
+
+def cleanup_f(f):
+	if os.path.isfile(f):
+		print(f)
+		return f
+	return None
+def cleanup():
+	c=confs.get_root_dir()
+	if os.path.isdir(c):
+		print("Would remove:")
+		f1=cleanup_f(confs.configs_filename)
+		f2=cleanup_f(listtor.config_filename)
+		print(c)
+		print("yes ?");
+		str = ""
+		while True:
+			x = sys.stdin.read(1) # reads one byte at a time, similar to getchar()
+			if x == '\n':
+				break
+			str += x
+		if str=="yes":
+			r=" removed"
+			if f1:
+				os.remove(f1)
+				print(f1+r)
+			if f2:
+				os.remove(f2)
+				print(f2+r)
+			if len(os.listdir(path=c))==0:
+				os.rmdir(c) #OSError if not empty
+				print(c.__str__()+r)
+			else:
+				print(c.__str__()+" is not empty.")
+		else:
+			print("expecting \"yes\"")
 
 if __name__ == "__main__":
 	main()
